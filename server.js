6db97361d5f36d3c769d19bdd3d1518d007361cd
SadikHasanRafi <sadikk.hasan@gmail.com>
2026-07-05 17:36:20 +0600

    enable DNS server configuration

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ import verifyToken from "./middleware/jwt.js";
 import User from "./models/DoctorRegistration/DoctorRegistration.js";
 
 
-// dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 dotenv.config();
 const app = express();
